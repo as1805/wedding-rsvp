337c566ac1a0bb7d6da410af81dadf3981f46cd9
Sadhana Ravoori <sadhana.ravoori@gmail.com>
2024-11-15 15:28:45 -0800

    added checkbox

--- app.py
+++ app.py
@@ -30,12 +30,16 @@ def set_background(png_file):
 st.set_page_config(page_title="Wedding RSVP", page_icon="💒")
 
 # Page title and introduction
-st.title("You're Invited!")
-st.header("Join us for the wedding of [Name] & [Name]")
-st.write("**Date:** [Date]")
-st.write("**Venue:** [Venue]")
+st.markdown("<h1 style='text-align: center;'>Wedding RSVP</h1>", unsafe_allow_html=True)
+st.header("Join us for the Wedding of Sadhana & Vikas")
+st.write("**Date:** December 14th and 15th")
+st.write("**Venue:** Poornima Convention Hall, Jayanagar, Bangalore")
 st.write(
-    "We are excited to celebrate this special day with you. Please let us know if you can join by filling out the form below.")
+    "We are excited to celebrate this special day with you!"
+)
+st.write(
+    "Please let us know if you can join by filling out the form below."
+)
 
 # Google Sheets integration setup
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
@@ -50,10 +54,12 @@ set_background('background.jpg')
 
 # RSVP form
 with st.form("rsvp_form"):
-    name = st.text_input("Name:", placeholder="Enter your full name", max_chars=100)
-    guests = st.number_input("Number of Guests:", min_value=1, max_value=10, step=1)
-    message = st.text_area("Special Requests / Dietary Restrictions:",
-                           placeholder="Any special requests or dietary needs")
+    name = st.text_input("**Name**", placeholder="Enter your full name", max_chars=100)
+    guests = st.number_input("**Number of Guests**", min_value=1, max_value=10, step=1)
+    st.write("**Reception** *14-December | 6:30PM onwards*")
+    attend_reception = st.checkbox("I will attend the Reception")
+    st.write("**Wedding** *15-December | 12:01PM Muhurthum*")
+    attend_wedding = st.checkbox("I will attend the Wedding")
     
     # Submit button
     submitted = st.form_submit_button("Submit RSVP")
@@ -61,8 +67,8 @@ with st.form("rsvp_form"):
     if submitted:
         if name:
             # Append the data to Google Sheets
-            gsheet.append_row([name, guests, message])
-            st.success("Thank you for your RSVP, " + name + "! We look forward to seeing you at the wedding.")
+            gsheet.append_row([name, guests, attend_reception, attend_wedding])
+            st.success("Thank you for your RSVP! We'll see you soon!")
         else:
             st.error("Please fill out all required fields.")
 
